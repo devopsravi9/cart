31a1763745454e0e1e4b18343e67172e2e23a95c
Ravi <ravi@gmail.com>
2022-08-10 14:47:25 +0530

    ec2 code

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,16 +8,6 @@ nodejs()
 
 
 
-
-
-
-
-
-
-
-
-
-
 // here in output you will get a dir named node_modules which generated after run npm install.
 // if u build the code we should remove that directory from before execute npm install.
 //the overall context is during rebuild the code all prev dir generated from prev build should be removed.
---
+++ ami.tf
@@ -0,0 +1,19 @@
+//here we creating an ami is making in part of continuous integaration. means
+//while we perform CI that should generate ami of this component. that is a plan here
+
+provider "aws" {
+  region      = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket    = "terraform-rk01"
+    key       = "ami/cart/terraform.tfstate"
+    region    = "us-east-1"
+  }
+}
+
+module "ami" {
+  source       = "github.com/devopsravi9/immutable-app-ami"
+  COMPONENT    = "cart"
+}
